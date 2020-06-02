b2e23abb621d9cae5844522d4b8f5d710508cf27
VaderSteve76 <alverson.s@yahoo.com>
2020-06-02 19:12:43 -0400

    Functions for examples of O(n) and O(n*n)

--- Big O/BigONotation.js
+++ Big O/BigONotation.js
@@ -46,3 +46,25 @@ addUpToTimer2(1000000000);
 let time2 = performance.now();
 // output = (time2 - time1) / 1000
 console.log(`Time elapsed: ${(time2 - time1) / 1000} seconds`);
+
+// Example O(n)
+function countUpAndDown(n) {
+  console.log("Going Up!");
+  for(let i=0; i<n; i++) {
+    console.log(i);
+  }
+  console.log("At the top!\nGoing Down...");
+  for(let j = n - 1; j>=0; j--) {
+    console.log(j);
+  }
+  console.log("Back Down. Bye!");
+};
+
+// Nested loop example for O(n * n)
+function printAllPairs(n) {
+  for(var i=0; i<n; i++) {
+    for(var j = 0; j < n; j++) {
+      console.log(i, j);
+    }
+  }
+};
