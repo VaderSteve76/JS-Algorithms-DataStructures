2ebcdb5613ae8b30e5da96ac33f9cd1dc8908d03
VaderSteve76 <alverson.s@yahoo.com>
2020-06-02 20:27:17 -0400

    Added more notes as well as some more examples of Big O notation

--- Big O/BigONotation.js
+++ Big O/BigONotation.js
@@ -68,3 +68,17 @@ function printAllPairs(n) {
     }
   }
 };
+
+// O(n)
+function logAtLeast5(n) {
+  for(var i=1; i<=Math.max(5, n); i++) {
+    console.log(i);
+  }
+};
+
+
+function logAtMost5(n) {
+  for(var i=1; i<=Math.min(5, n); i++) {
+    console.log(i);
+  }
+};
