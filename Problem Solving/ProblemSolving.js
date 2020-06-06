b1245e6864c20d4e0f9c52b2ba10c430ff804372
VaderSteve76 <alverson.s@yahoo.com>
2020-06-06 14:32:43 -0400

    Added some notes as well as an example function

---
+++ Problem Solving/ProblemSolving.js
@@ -0,0 +1,14 @@
+// Write a function that takes two numbers and returns the sum
+
+// 1. Can I restate the problem in my own words?
+// 2. What are the inputs that go into the problem?
+// 3. What are the outputs that should come from the solution?
+// 4. Can the outputs be determined from the inputs? (do I have enough info??)
+// 5. How should I label the important pieces of data?
+
+
+function add(a, b) {
+  return a + b;
+};
+
+add(5, 5);
