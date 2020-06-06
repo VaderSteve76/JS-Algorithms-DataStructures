8626f3c0b09beb915afcf13fffb0d467c928fcd0
VaderSteve76 <alverson.s@yahoo.com>
2020-06-06 15:58:12 -0400

    Added notes and function refactor examples without regular expression

--- Problem Solving/ProblemSolving.js
+++ Problem Solving/ProblemSolving.js
@@ -4,24 +4,21 @@
 // 4. Can the outputs be determined from the inputs? (do I have enough info??)
 // 5. How should I label the important pieces of data?
 
-
 // Write a function that takes two numbers and returns the sum
 function add(a, b) {
   return a + b;
-};
+}
 
 add(5, 5);
-
 
 // Write a function that takes in a string and counts all chars
 
 // make obj to return at end
 // loop over str, for each char...
-   // if char is a num/letter AND is a key in obj, add +1 to count
-   // if char is a num/letter AND is not in obj, add it to obj and set value to 1
-   // if char is something else (space, period, etc.) do nothing
+// if char is a num/letter AND is a key in obj, add +1 to count
+// if char is a num/letter AND is not in obj, add it to obj and set value to 1
+// if char is something else (space, period, etc.) do nothing
 // return obj at end
-
 
 function countChars(str) {
   var a = str.split("");
@@ -30,19 +27,48 @@ function countChars(str) {
     obj[s] = (obj[s] || 0) + 1;
   });
   return obj;
-};
+}
 console.log(countChars("fishing"));
 
 
 function charCount(str) {
-  var result = {};
-  for(var i=0; i<str.length; i++) {
+  var obj = {};
+  for (var i = 0; i < str.length; i++) {
     var char = str[i].toLowerCase();
-    if(result[char] > 0) {
-      result[char]++;
-    } else {
-      result[char] = 1;
+    if (/[a-z0-9]/.test(char)) {
+      if (obj[char] > 0) {
+        obj[char]++;
+      } else {
+        obj[char] = 1;
+      }
     }
   }
-  return result;
-};
+  return obj;
+}
+
+
+// Refactored example
+function charCount(str) {
+  var obj = {};
+  for (var char of str) {
+    char = char.toLowerCase();
+    if (isAlphaNumeric(char)) {
+      obj[char] = ++obj[char] || 1;
+    }
+  }
+  return obj;
+}
+
+
+function isAlphaNumeric(char) {
+  var code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) &&
+    !(code > 64 && code < 91) &&
+    !(code > 96 && code < 123)
+  ) {
+    return false;
+  }
+  return true;
+}
+charCodeAt(0);
