760a65ae83ba2fbfedd214bbc01ce14bd2bea3ab
VaderSteve76 <alverson.s@yahoo.com>
2020-06-06 15:19:25 -0400

    Added a second function example for char count

--- Problem Solving/ProblemSolving.js
+++ Problem Solving/ProblemSolving.js
@@ -14,6 +14,15 @@ add(5, 5);
 
 
 // Write a function that takes in a string and counts all chars
+
+// make obj to return at end
+// loop over str, for each char...
+   // if char is a num/letter AND is a key in obj, add +1 to count
+   // if char is a num/letter AND is not in obj, add it to obj and set value to 1
+   // if char is something else (space, period, etc.) do nothing
+// return obj at end
+
+
 function countChars(str) {
   var a = str.split("");
   var obj = {};
@@ -23,3 +32,17 @@ function countChars(str) {
   return obj;
 };
 console.log(countChars("fishing"));
+
+
+function charCount(str) {
+  var result = {};
+  for(var i=0; i<str.length; i++) {
+    var char = str[i].toLowerCase();
+    if(result[char] > 0) {
+      result[char]++;
+    } else {
+      result[char] = 1;
+    }
+  }
+  return result;
+};
