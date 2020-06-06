a88d2865d44e53000e3d57c3d3b86484b721545f
VaderSteve76 <alverson.s@yahoo.com>
2020-06-06 14:48:57 -0400

    Added function example for str count

--- Problem Solving/ProblemSolving.js
+++ Problem Solving/ProblemSolving.js
@@ -1,5 +1,3 @@
-// Write a function that takes two numbers and returns the sum
-
 // 1. Can I restate the problem in my own words?
 // 2. What are the inputs that go into the problem?
 // 3. What are the outputs that should come from the solution?
@@ -7,8 +5,21 @@
 // 5. How should I label the important pieces of data?
 
 
+// Write a function that takes two numbers and returns the sum
 function add(a, b) {
   return a + b;
 };
 
 add(5, 5);
+
+
+// Write a function that takes in a string and counts all chars
+function countChars(str) {
+  var a = str.split("");
+  var obj = {};
+  a.forEach(function(s) {
+    obj[s] = (obj[s] || 0) + 1;
+  });
+  return obj;
+};
+console.log(countChars("fishing"));
