062ca484fe91816fc57e766d7bf2a2688660352e
VaderSteve76 <alverson.s@yahoo.com>
2020-06-29 19:04:40 -0400

    Added function example for frequency counter

--- Problem Solving/ProblemSolving.js
+++ Problem Solving/ProblemSolving.js
@@ -72,3 +72,19 @@ function isAlphaNumeric(char) {
   return true;
 }
 charCodeAt(0);
+
+
+// Frequency Counter example
+function same(arr1, arr2) {
+  if(arr1.length !== arr2.length) {
+    return false;
+  }
+  for(let i=0; i<arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    if(correctIndex === -1) {
+      return false;
+    }
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+};
