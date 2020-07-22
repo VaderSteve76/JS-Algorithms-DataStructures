9905f024eb98be27484877e8e8d05b563cf674a2
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-07-22 09:39:03 -0400

    Added a refactored version of funtion to find the first sum of 0

--- Problem Solving/ProblemSolving.js
+++ Problem Solving/ProblemSolving.js
@@ -136,7 +136,7 @@ function validAnagram(first, second) {
 };
 
 
-function sumZero(arr) {
+function sumZero1(arr) {
   for(let i=0; i < arr.length; i++) {
     for(let j = i+1; j < arr.length; j++) {
       if(arr[i] + arr[j] === 0) {
@@ -145,4 +145,21 @@ function sumZero(arr) {
     }
   }
 };
-sumZero([-4,-3,-2,-1,0,1,2,5]);
+sumZero1([-4,-3,-2,-1,0,1,2,5]);
+
+
+function sumZero2(arr) {
+  let left = 0;
+  let right = arr.length -1;
+  while(left < right) {
+    let sum = arr[left] + arr[right];
+    if(sum === 0) {
+      return [arr[left], arr[right]];
+    } else if(sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+};
+sumZero2([-4,-3,-2,1-,0,1,2,3,10]);
