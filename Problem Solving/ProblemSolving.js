2ae098d9aab733dfc13ae18a6645d437333d0979
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-07-22 08:53:18 -0400

    Added function example to compare strings and check for anagram

--- Problem Solving/ProblemSolving.js
+++ Problem Solving/ProblemSolving.js
@@ -113,3 +113,23 @@ function same(arr1, arr2) {
   }
   return true;
 }
+
+
+function validAnagram(first, second) {
+  if(first.length !== second.length) {
+    return false;
+  }
+  const lookup = {};
+  for(let i=0; i < first.length; i++) {
+    let letter = first[i];
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+  }
+  for(let i=0; i < second.length; i++) {
+    let letter = second[i];
+    if(!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -=1;
+    }
+  }
+};
