b2b2ce3364e62a032ea553a549fbd507b5da3902
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-07-26 18:21:09 -0400

    Added a factorial function example

--- Recursion/Recursion.js
+++ Recursion/Recursion.js
@@ -21,3 +21,15 @@ sumRange(3);
   // return 3 + sumRange(2)
   //             return 2 + sumRange(1)
   //                         return 1
+
+
+// Factorial function
+// 4 * 3 * 2 * 1 || 6 * 5 * 4 * 3 * 2 * 1
+function factorial(num) {
+  let total = 1;
+  for(let i =num; i > 1; i--) {
+    total *= i;
+  }
+  return total;
+};
+factorial(5); // 5 * 4 * 3 * 2 * 1
