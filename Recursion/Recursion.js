1b4bc330c41b37365fb5512daea3fb5c2e40c163
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-07-27 20:23:48 -0400

    Added some function examples of helper method recursion

--- Recursion/Recursion.js
+++ Recursion/Recursion.js
@@ -41,3 +41,40 @@ function factorial(num) {
   return num * factorial(num-1);
 };
 factorial(5); // 5 * 4 * 3 * 2 * 1 = 120
+
+
+// Helper Method Recursion
+function outer(input) {
+  var outerScopedVariable = []
+
+  function helper(helperInput) {
+    // modify the outerScopedVariable
+    helper(helperInput--)
+  }
+
+  helper(input)
+
+  return outerScopedVariable;
+
+};
+
+// Another example of helper method
+function collectOddValues(arr) {
+
+  let result = [];
+
+  function helper(helperInput) {
+    if(helperInput.length === 0) {
+      return;
+    }
+
+    if(helperInput[0] % 2 !== 0) {
+      result.push(helperInput[0])
+    }
+  }
+
+  helper(arr);
+
+  return result;
+  
+};
