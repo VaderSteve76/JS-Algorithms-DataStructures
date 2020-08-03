4cc6a52dba191bf8a26918535029fe8703745d8c
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-08-03 18:14:52 -0400

    Added a refactored version of binarySearch

--- Searching Algorithms/Search.js
+++ Searching Algorithms/Search.js
@@ -30,3 +30,18 @@ function binarySearch(arr, elem) {
 };
 
 binarySearch([2,5,6,9,13,15,28,30], 15);// [0,3,7]  [4,5,7]  5 = 15 value looking for
+
+
+// refactored binarySearch
+function binarySearch(arr, elem) {
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2);
+  while(arr[middle] !== elem && start <= end) {
+    if(elem < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === elem ? middle : -1;
+};
+binarySearch([2,5,6,9,13,15,28,30], 15);
