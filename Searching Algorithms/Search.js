8e913149de70bb6981077ff7329c038196297ba6
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-08-04 22:38:49 -0400

    Added naiveSearch example function

--- Searching Algorithms/Search.js
+++ Searching Algorithms/Search.js
@@ -45,3 +45,24 @@ function binarySearch(arr, elem) {
   return arr[middle] === elem ? middle : -1;
 };
 binarySearch([2,5,6,9,13,15,28,30], 15);
+
+
+// Naive string search
+function naiveSearch(long, short) {
+  var count = 0;
+  for(var i = 0; i < long.length; i++) {
+    for(var j = 0; j < short.length; j++) {
+      if(short[j] !== long[i+j]) {
+        console.log("Break!");
+        break;
+      }
+      if(j === short.length - 1) {
+        console.log("Found One!");
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+naiveSearch("lorie loled", "lol") 
