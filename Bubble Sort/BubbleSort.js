d20cbe2bd6a2baa7daf9331f7c593afd477644ee
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-08-06 16:34:06 -0400

    Added bubbleSort function that stops iterating after sort

--- Bubble Sort/BubbleSort.js
+++ Bubble Sort/BubbleSort.js
@@ -31,3 +31,25 @@ function BubbleSort(arr) {
 };
 
 BubbleSort([37,45,29,8]);
+
+
+// function that doesn't keep iterating after sort
+function BubbleSort(arr) {
+  var noSwaps;
+  for(var i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for(var j = 0; j < i - 1; j++) {
+      if(arr[j] > arr[i]) {
+        //Swap
+        var temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        noSwaps = false;
+      }
+    }
+    if(noSwaps) break;
+  }
+  return arr;
+};
+
+BubbleSort([37,45,29,8]);
