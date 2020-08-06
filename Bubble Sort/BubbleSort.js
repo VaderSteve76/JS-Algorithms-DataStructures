667870e9f6507ead129adb3287d26cac9ae05b5a
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-08-05 23:50:27 -0400

    Added bubbleSort starting from end of array

--- Bubble Sort/BubbleSort.js
+++ Bubble Sort/BubbleSort.js
@@ -13,3 +13,21 @@ function bubbleSort(arr) {
 };
 
 bubbleSort([37,45,29,8]);
+
+
+// bubbleSort starting at end of array
+function BubbleSort(arr) {
+  for(var i = arr.length; i > 0; i--) {
+    for(var j = 0; j < i - 1; j++) {
+      if(arr[j] > arr[i]) {
+        //Swap
+        var temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+BubbleSort([37,45,29,8]);
