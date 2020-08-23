4e2099d6d92653abf8723ba08c0e2cb64300e842
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-08-22 22:54:19 -0400

    Added insertionSort example with while loop

--- Insertion Sort/InsertionSort.js
+++ Insertion Sort/InsertionSort.js
@@ -11,3 +11,21 @@ function insertionSort(arr) {
 };
 
 insertionSort([2,1,9,76,4]);
+
+
+// insertionSort with while loop
+function insertionSort(arr) {
+  let n = arr.length;
+      for (let i = 1; i < n; i++) {
+          let current = arr[i];
+          let j = i-1; 
+          while ((j > -1) && (current < arr[j])) {
+              arr[j+1] = arr[j];
+              j--;
+          }
+          arr[j+1] = current;
+      }
+  return arr;
+}
+
+insertionSort([2,1,9,76,4]);
