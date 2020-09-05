51bb7f0312388ca85cad6c9551037f3394c22c40
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-09-05 17:36:12 -0400

    Added examples of class instance methods

--- Data Structures/Classes.js
+++ Data Structures/Classes.js
@@ -4,12 +4,41 @@
 
 // Class syntax
 class Student {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, year) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.grade = year;
+    this.tardies = 0;
+    this.scores = [];
   }
+  // instance methods
+  fullName() {
+    return `Your full name is ${this.firstName} ${this.lastName}`;
+  }
+  markLate() {
+    this.tardies += 1;
+    if(this.tardies >= 3) {
+      return "YOU ARE EXPELLED!!!!";
+    }
+    return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
+  }
+  addScore(score) {
+    this.scores.push(score);
+    return this.scores;
+  }
+  calculateAvg() {
+    let sum = this.scores.reduce(function(a,b) {return a+b});
+    return sum/this.scores.length;
+  }
+
 }
 
 // creating objects from classes, using the "new" keyword
-let firstStudent = new Student("Joe", "Blow");
-let secondStudent = new Student("John", "Smith");
+let firstStudent = new Student("Joe", "Blow", 4);
+let secondStudent = new Student("John", "Smith", 2);
+
+// calling instance method
+firstStudent.fullName(); // "Joe Blow"
+firstStudent.markLate();
+firstStudent.addScore(94);
+firstStudent.calculateAvg();
