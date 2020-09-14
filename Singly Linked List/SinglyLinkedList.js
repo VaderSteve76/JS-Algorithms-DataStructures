700eeeb49f116f8ee0b82d5d211a0b6b214f5f64
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-09-14 17:14:38 -0400

    Added insert method example for singly linked list

--- Singly Linked List/SinglyLinkedList.js
+++ Singly Linked List/SinglyLinkedList.js
@@ -90,6 +90,19 @@ class SinglyLinkedList {
     return false;
   }
 
+  insert(index, val) {
+    if(index < 0 || index > this.length) return false;
+    if(index === this.length) this.push(val);
+    if(index === 0) return this.unshift(val);
+    var newNode = new node(val);
+    var prev = this.get(index -1);
+    var temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
+
 };
 
 var list = new SinglyLinkedList();
