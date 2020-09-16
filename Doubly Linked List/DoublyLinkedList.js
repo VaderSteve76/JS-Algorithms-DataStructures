a1f60898e3869047e90f9c9a2067df2044070e30
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-09-16 17:42:12 -0400

    Added node & doubly linked list classes

---
+++ Doubly Linked List/DoublyLinkedList.js
@@ -0,0 +1,15 @@
+class node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
