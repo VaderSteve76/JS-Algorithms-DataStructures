7fbc97d8cb74c31034ccc26ae351ef78003cb0bd
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-09-27 16:12:44 -0400

    Added notes on enqueue method for queues

--- Queues/Queues.js
+++ Queues/Queues.js
@@ -1,14 +1,14 @@
+class node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class Queue {
   constructor() {
     this.first = null;
     this.last = null;
     this.size = 0;
-  }
-}
-
-class node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
   }
 }
