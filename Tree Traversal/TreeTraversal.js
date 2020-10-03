702e7958c18d141546d8cd08b8588a290708c776
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-10-03 18:10:16 -0400

    Added breadth first search example for tree traversal

---
+++ Tree Traversal/TreeTraversal.js
@@ -0,0 +1,73 @@
+class node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    var newNode = new node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    var current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  BFS(data, queue) {
+    var node = this.root;
+    data = [];
+    queue = [];
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+}
+
+// bst example
+//       10
+//   5       13
+// 2  7   11   16
+
+// binary tree setup
+var tree = new BinarySearchTree();
+tree.root = new node(10);
+tree.root.right = new node(15);
+tree.root.left = new node(7);
+tree.root.left.right = new node(9);
+
+// inserting
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
