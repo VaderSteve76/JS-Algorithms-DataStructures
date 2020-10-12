3f643f37eb093900669de11dd219c4ab7698c1af
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2020-10-12 16:09:48 -0400

    Added refactored example of hash table

--- Hash Tables/HashTables.js
+++ Hash Tables/HashTables.js
@@ -7,3 +7,15 @@ function hash(key, arrayLen) {
   }
   return total;
 }
+
+
+function hash(key, arrayLen) {
+  let total = 0;
+  let weird_prime = 31;
+  for(let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96;
+    total = (total * weird_prime + value) % arrayLen;
+  }
+  return total;
+}
