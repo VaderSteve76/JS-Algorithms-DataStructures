cd217f2160697f79c26e9713eac219f298cd9f08
Steve Alverson <alverson.s@yahoo.com>
2020-10-17 13:44:20 -0400

    Added edge method example for graphs

--- Graphs/Graphs.js
+++ Graphs/Graphs.js
@@ -7,10 +7,22 @@ class Graph {
     if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
 
+  addEdge(v1,v2) {
+    this.adjacencyList[v1].push[v2];
+    this.adjacencyList[v2].push[v1];
+  }
+
 }
 
-var g = new Graph();
+let g = new Graph();
+// add vertices
 g.addVertex("Tokyo");
 g.addVertex("New York");
 g.addVertex("Paris");
 g.addVertex("London");
+
+// add edges
+g.addEdge("New York", "London");
+g.addEdge("London", "Paris");
+g.addEdge("London", "Tokyo");
+g.addEdge("New York", "Paris");
